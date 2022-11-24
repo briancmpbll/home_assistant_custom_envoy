b567f35520a81479990631ea88ff4b2949bb6413
Graham McMicken <graham@mcmicken.ca>
2022-11-23 23:29:05 -0800

    Add sensor definitions for grid_import and grid_export

--- custom_components/enphase_envoy_custom/const.py
+++ custom_components/enphase_envoy_custom/const.py
@@ -105,6 +105,20 @@ SENSORS = (
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.ENERGY
     ),
+    SensorEntityDescription(
+        key="grid_import",
+        name="Grid Energy Imported Counter",
+        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        device_class=SensorDeviceClass.ENERGY,
+    ),
+    SensorEntityDescription(
+        key="grid_export",
+        name="Grid Energy Exported Counter",
+        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        device_class=SensorDeviceClass.ENERGY,
+    ),
 )
 
 BATTERY_ENERGY_DISCHARGED_SENSOR = SensorEntityDescription(
